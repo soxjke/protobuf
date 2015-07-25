39bb0fc3e87900c805225c4859fbf539ba93953c
Petro Korienev <soxjke@gmail.com>
2015-07-25 16:28:21 +0300

    Few more improvements to install script

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 system("for FILENAME in *.proto; do [[ -f ${FILENAME} ]] && mv $FILENAME APS$FILENAME; done")
 system("protoc --plugin=/usr/local/bin/protoc-gen-objc --proto_path=.:protobuf-objc/src/compiler/ *.proto --objc_out=\"./\"")
 system("for FILENAME in *; do [[ -f ${FILENAME} ]] && mv $FILENAME \"`echo $FILENAME | sed s/.pb//`\"; done")
