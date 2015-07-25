91eb4f11add5c28c5beb262576ba84b98f611370
Petro Korienev <soxjke@gmail.com>
2015-07-25 16:19:56 +0300

    Few more improvements to install script

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 system("for FILENAME in *.proto; do [[ -f ${FILENAME} ]] && mv $FILENAME APS$FILENAME; done")
 system("protoc --plugin=/usr/local/bin/protoc-gen-objc --proto_path=.:protobuf-objc/src/compiler/ *.proto --objc_out=\"./\"")
 system("for FILENAME in *; do [[ -f ${FILENAME} ]] && mv $FILENAME \"`echo $FILENAME | sed s/.pb//`\"; done")
-system("find . -iname '*.h' -print0 | while read -d $'\\0' -r file ; do sed '/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
-system("find . -iname '*.m' -print0 | while read -d $'\\0' -r file ; do sed '/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
+system("find . -iname '*.h' -print0 | while read -d $'\\0' -r file ; do sed 's/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
+system("find . -iname '*.m' -print0 | while read -d $'\\0' -r file ; do sed 's/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
 system("rm install.rb")
