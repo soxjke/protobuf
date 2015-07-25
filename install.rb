f5e21145e49cd61f8c6ce941c751d6a87762f37f
Petro Korienev <soxjke@gmail.com>
2015-07-25 16:25:58 +0300

    Few more improvements to install script

--- install.rb
+++ install.rb
@@ -1,6 +1,10 @@
 system("for FILENAME in *.proto; do [[ -f ${FILENAME} ]] && mv $FILENAME APS$FILENAME; done")
 system("protoc --plugin=/usr/local/bin/protoc-gen-objc --proto_path=.:protobuf-objc/src/compiler/ *.proto --objc_out=\"./\"")
 system("for FILENAME in *; do [[ -f ${FILENAME} ]] && mv $FILENAME \"`echo $FILENAME | sed s/.pb//`\"; done")
-system("find . -iname '*.h' -print0 | while read -d $'\\0' -r file ; do sed 's/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
-system("find . -iname '*.m' -print0 | while read -d $'\\0' -r file ; do sed 's/.pb.h\"/.h\"/' \"$file\" > ./tmp\ & mv ./tmp \"$file\"\; done")
+Dir.glob('./*.{h,m}') do |sourceFile|
+	File.open("config.tmp", "w") do |io|
+		io << File.read(sourceFile).gsub(/\.pb\.h\"/, '.h"')
+	end
+	FileUtils.mv("config.tmp", sourceFile)	
+end
 system("rm install.rb")
