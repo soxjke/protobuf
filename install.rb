67d28b926db34e6596807e0134c8ef3c53c20e19
Petro Korienev <soxjke@gmail.com>
2015-07-25 16:48:39 +0300

    Added building up of root import

--- install.rb
+++ install.rb
@@ -3,10 +3,16 @@ require 'fileutils'
 system("for FILENAME in *.proto; do [[ -f ${FILENAME} ]] && mv $FILENAME APS$FILENAME; done")
 system("protoc --plugin=/usr/local/bin/protoc-gen-objc --proto_path=.:protobuf-objc/src/compiler/ *.proto --objc_out=\"./\"")
 system("for FILENAME in *; do [[ -f ${FILENAME} ]] && mv $FILENAME \"`echo $FILENAME | sed s/.pb//`\"; done")
-Dir.glob('./*.{h,m}') do |sourceFile|
-	File.open("config.tmp", "w") do |io|
-		io << File.read(sourceFile).gsub(/\.pb\.h\"/, '.h"')
+File.open("Header.tmp", "w") do |headerIO|
+	Dir.glob('./*.{h,m}') do |sourceFile|
+		File.open("config.tmp", "w") do |io|
+			io << File.read(sourceFile).gsub(/\.pb\.h\"/, '.h"')
+		end
+		FileUtils.mv("config.tmp", sourceFile)	
 	end
-	FileUtils.mv("config.tmp", sourceFile)	
+	if sourceFile.end_with? ".h" then
+        headerIO << "#import \"" + sourceFile + "\""
+	end
 end
+FileUtils.mv("Header.tmp", "AppspectorProtobuf.h")
 system("rm install.rb")
